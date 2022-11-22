c91989f5995a49d0fad04b899a19b78535ebb02b
Tony Brown <tb148@proton.me>
2022-11-22 22:44:18 +0800

    fix: add max-width

--- src/App.svelte
+++ src/App.svelte
@@ -1,146 +1,144 @@
 <script lang="ts">
-	import Well from "./Well.svelte";
-	import { Direction, type State } from "./lib/state";
-	import { empty_state, offset } from "./lib/state";
-	import { new_state, rotate_state, offset_state } from "./lib/move";
+  import Well from "./Well.svelte";
+  import { Direction, type State } from "./lib/state";
+  import { empty_state, offset } from "./lib/state";
+  import { new_state, rotate_state, offset_state } from "./lib/move";
 
-	let history: State[] = [empty_state()];
-	$: state = history.at(-1);
+  let history: State[] = [empty_state()];
+  $: state = history.at(-1);
 
-	const reset = () => {
-		history = [empty_state()];
-	};
-	const undo = () => {
-		if (history.length > 1) {
-			history = history.slice(0, -1);
-		}
-	};
-	const move = (direction: Direction) => {
-		const result = offset_state(state, offset[direction]);
-		if (result != state) {
-			history = [...history, result];
-		} else if (direction == Direction.Down && result.piece != null) {
-			history = [...history, new_state(state)];
-		}
-	};
-	const rotate = (direction: Direction) => {
-		const result = rotate_state(state, direction);
-		if (result != state) {
-			history = [...history, result];
-		}
-	};
-	const key_press = (event: KeyboardEvent) => {
-		if (event.code == "KeyA") {
-			move(Direction.Left);
-		}
-		if (event.code == "KeyS") {
-			move(Direction.Down);
-		}
-		if (event.code == "KeyD") {
-			move(Direction.Right);
-		}
-		if (event.code == "KeyK") {
-			rotate(Direction.Left);
-		}
-		if (event.code == "KeyL") {
-			rotate(Direction.Right);
-		}
-		if (event.code == "Semicolon") {
-			rotate(Direction.Down);
-		}
-	};
+  const reset = () => {
+    history = [empty_state()];
+  };
+  const undo = () => {
+    if (history.length > 1) {
+      history = history.slice(0, -1);
+    }
+  };
+  const move = (direction: Direction) => {
+    const result = offset_state(state, offset[direction]);
+    if (result != state) {
+      history = [...history, result];
+    } else if (direction == Direction.Down && result.piece != null) {
+      history = [...history, new_state(state)];
+    }
+  };
+  const rotate = (direction: Direction) => {
+    const result = rotate_state(state, direction);
+    if (result != state) {
+      history = [...history, result];
+    }
+  };
+  const key_press = (event: KeyboardEvent) => {
+    if (event.code == "KeyA") {
+      move(Direction.Left);
+    }
+    if (event.code == "KeyS") {
+      move(Direction.Down);
+    }
+    if (event.code == "KeyD") {
+      move(Direction.Right);
+    }
+    if (event.code == "KeyK") {
+      rotate(Direction.Left);
+    }
+    if (event.code == "KeyL") {
+      rotate(Direction.Right);
+    }
+    if (event.code == "Semicolon") {
+      rotate(Direction.Down);
+    }
+  };
 </script>
 
 <svelte:window on:keydown={key_press} />
 <template>
-	<div class="game">
-		<div class="main">
-			<div class="well"><Well {state} /></div>
-			<div class="about">
-				<div class="text">Score: {state.score}</div>
-				<div class="text">
-					Controls:
-					<br />A: Move Left
-					<br />S: Move Down
-					<br />D: Move Right
-					<br />K: Rotate CCW
-					<br />L: Rotate CW
-					<br />;: Rotate Twice
-				</div>
-				<div class="space" />
-				<div class="text">
-					<a href="https://github.com/tb148/tetris-legacy"
-						>Source Code</a
-					>
-				</div>
-				<div class="text">
-					Inspired by <a href="https://qntm.org/hatetris">Hatetris</a
-					>.
-				</div>
-			</div>
-		</div>
-		<div class="buttons">
-			<button on:click={reset}>Reset</button>
-			<button on:click={undo} disabled={history.length == 1}>Undo</button>
-		</div>
-	</div>
+  <div class="game">
+    <div class="main">
+      <div class="well"><Well {state} /></div>
+      <div class="about">
+        <div class="text">Score: {state.score}</div>
+        <div class="text">
+          Controls:
+          <br />A: Move Left
+          <br />S: Move Down
+          <br />D: Move Right
+          <br />K: Rotate CCW
+          <br />L: Rotate CW
+          <br />;: Rotate Twice
+        </div>
+        <div class="space" />
+        <div class="text">
+          <a href="https://github.com/tb148/tetris-legacy">Source Code</a>
+        </div>
+        <div class="text">
+          Inspired by <a href="https://qntm.org/hatetris">Hatetris</a>.
+        </div>
+      </div>
+    </div>
+    <div class="buttons">
+      <button on:click={reset}>Reset</button>
+      <button on:click={undo} disabled={history.length == 1}>Undo</button>
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
-	.game {
-		width: 20em;
-		display: flex;
-		flex-flow: column;
-		gap: 1em;
-		.main {
-			display: flex;
-			flex: none;
-			flex-flow: row;
-			gap: 1em;
+  .game {
+    display: flex;
+    flex-flow: column;
+    gap: 1em;
+    max-width: 100vw;
+    width: 20em;
+    .main {
+      display: flex;
+      flex: none;
+      flex-flow: row;
+      gap: 1em;
 
-			.well {
-				flex: none;
-			}
+      .well {
+        flex: none;
+      }
 
-			.about {
-				display: flex;
-				flex: auto;
-				flex-flow: column;
-				.text {
-					flex: none;
-					margin-bottom: 1em;
-				}
-				.text:last-child {
-					margin-bottom: 0;
-				}
-				.space {
-					flex: auto;
-				}
-			}
-		}
-		.buttons {
-			display: flex;
-			gap: 1em;
-			button {
-				background-color: blue;
-				border: 0;
-				color: white;
-				height: 3em;
-				width: 100%;
-			}
-			button:hover {
-				background-color: #009;
-			}
-			button:active {
-				background-color: red;
-			}
-			button[disabled] {
-				background-color: #999;
-				cursor: default;
-			}
-			:focus {
-				outline: 2px solid black;
-			}
-		}
-	}
+      .about {
+        display: flex;
+        flex: auto;
+        flex-flow: column;
+        .text {
+          flex: none;
+          margin-bottom: 1em;
+        }
+        .text:last-child {
+          margin-bottom: 0;
+        }
+        .space {
+          flex: auto;
+        }
+      }
+    }
+    .buttons {
+      display: flex;
+      gap: 1em;
+      button {
+        background-color: blue;
+        border: 0;
+        color: white;
+        height: 3em;
+        width: 100%;
+      }
+      button:hover {
+        background-color: #009;
+      }
+      button:active {
+        background-color: red;
+      }
+      button[disabled] {
+        background-color: #999;
+        cursor: default;
+      }
+      :focus {
+        outline: 2px solid black;
+      }
+    }
+  }
 </style>
--- src/Well.svelte
+++ src/Well.svelte
@@ -1,61 +1,61 @@
 <script lang="ts">
-	import type { State } from "./lib/state";
-	import display_well from "./lib/well";
-	export let state: State;
-	$: well = display_well(state);
+  import type { State } from "./lib/state";
+  import display_well from "./lib/well";
+  export let state: State;
+  $: well = display_well(state);
 </script>
 
 <template>
-	<div class="tiles">
-		{#each well as flat, i}
-			{#each flat as tile, j}
-				<div class="tile tile-{tile} tile-flat{i} tile-col{j}" />
-			{/each}
-		{/each}
-	</div>
+  <div class="tiles">
+    {#each well as flat, i}
+      {#each flat as tile, j}
+        <div class="tile tile-{tile} tile-flat{i} tile-col{j}" />
+      {/each}
+    {/each}
+  </div>
 </template>
 
 <style lang="scss">
-	.tiles {
-		display: grid;
-		grid-template-columns: repeat(10, 1fr);
-		grid-template-rows: repeat(20, 1fr);
+  .tiles {
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+    grid-template-rows: repeat(20, 1fr);
 
-		.tile {
-			width: 1.2em;
-			height: 1.2em;
-			padding: 0;
-		}
+    .tile {
+      width: 1.2em;
+      height: 1.2em;
+      padding: 0;
+    }
 
-		.tile-empty {
-			background-color: black;
-		}
-		.tile-empty:hover {
-			background-color: #333;
-		}
+    .tile-empty {
+      background-color: black;
+    }
+    .tile-empty:hover {
+      background-color: #333;
+    }
 
-		.tile-alive {
-			background-color: red;
-		}
-		.tile-alive:hover {
-			background-color: #f33;
-		}
+    .tile-alive {
+      background-color: red;
+    }
+    .tile-alive:hover {
+      background-color: #f33;
+    }
 
-		.tile-dead {
-			background-color: blue;
-		}
-		.tile-dead:hover {
-			background-color: #33f;
-		}
+    .tile-dead {
+      background-color: blue;
+    }
+    .tile-dead:hover {
+      background-color: #33f;
+    }
 
-		.tile-flat15 {
-			border-top: 1px solid red;
-		}
+    .tile-flat15 {
+      border-top: 1px solid red;
+    }
 
-		@for $i from 0 to 20 {
-			.tile-flat#{$i} {
-				grid-row: 20 - $i;
-			}
-		}
-	}
+    @for $i from 0 to 20 {
+      .tile-flat#{$i} {
+        grid-row: 20 - $i;
+      }
+    }
+  }
 </style>
